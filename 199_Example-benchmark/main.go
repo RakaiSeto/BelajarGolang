f047e121d38bd18edc985c568bad5c151d438572
Rakai Seto <mamangrakai@gmail.com>
2021-12-19 14:43:41 +0700

    tes

--- 199_Example-benchmark/main.go
+++ 199_Example-benchmark/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+
+	mystr "github.com/RakaiSeto/belajarGolang/199_Example-benchmark/myStr"
+)
+
+const s = "Exercitation enim qui adipisicing fugiat do. Consectetur ad do ea nostrud mollit irure ex id aliquip irure in cupidatat ut. Lorem consectetur do commodo nisi voluptate reprehenderit proident reprehenderit ad. Sit labore deserunt ad consectetur laborum minim in laboris."
+
 func main() {
-	
+	xs := strings.Split(s, " ")
+
+	for _, v := range xs {
+		fmt.Println(v)
+	}
+
+	fmt.Printf("\n%s\n", mystr.Cat(xs))
+	fmt.Printf("\n%s\n\n", mystr.Join(xs))
 }
---
+++ 199_Example-benchmark/myStr/go.mod
@@ -0,0 +1,3 @@
+module github.com/RakaiSeto/BelajarGolang/199_Example-benchmark/myStr
+
+go 1.17
